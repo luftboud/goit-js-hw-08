4f3e5b55ef2228879f48fc31b260457949374a27
luftboud <ijamaharyta@gmail.com>
2023-04-17 21:17:24 +0300

    task 2 - complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function updateLocalStorage(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+  console.log(data);
+}
+
+player.on('timeupdate', throttle(updateLocalStorage, 1000));
+
+try {
+  player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+} catch (err) {
+  console.log(err);
+}
