277213e5bc54aa10d8efd70048060dbda4070172
luftboud <ijamaharyta@gmail.com>
2023-04-22 17:53:09 +0300

    task 3 - done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const STORAGE_KEY = 'feedback-form-state';
+const localStorageInput = {};
+
+checkSave();
+form.addEventListener('input', throttle(updateLocalStorage, 500));
+form.addEventListener('submit', submitForm);
+
+function updateLocalStorage(event) {
+  event.preventDefault();
+  const data = event.target.value;
+  localStorageInput[event.target.name] = data;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(localStorageInput));
+}
+
+function checkSave() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    const { email, message } = savedData;
+    if (savedData.email) {
+      form.email.value = email;
+    }
+    if (savedData.message) {
+      form.message.value = message;
+    }
+  }
+}
+function submitForm(event) {
+  event.preventDefault();
+  console.log(localStorageInput);
+  event.currentTarget.reset();
+  localStorage.clear;
+}
